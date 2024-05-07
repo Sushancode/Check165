cf1f3ebe5004a4416429b6084af8b7bd3d8bf2e5
Sushancoder <135816346+Sushancoder@users.noreply.github.com>
2024-05-07 18:40:07 +0530

    Create arraytest.py
    Testing how arrays work in python by modules

---
+++ arraytest.py
@@ -0,0 +1,887 @@
+# import array
+
+# my_array = array.array('i', [1, 2, 3, 4, 5])
+# print(my_array.tolist())
+
+# print(my_array[0])      # Output: 1
+# my_array[2] = 10
+# print(my_array)         # Output: array('i', [1, 2, 10, 4, 5])
+# print(len(my_array))    # Output: 5
+
+# def factorial(a):
+#     if a < 1:
+#         print(a)
+#     else :
+#         a = a * (a-1)
+#         a = a - 1
+#         factorial(a)
+
+# factorial(3)
+# print('hey')
+
+
+# # runnitg a binary code converter
+# st = "hello world bro this is one of the most popular and important world that I have ever visited..."
+# r = ' '.join(format(ord(x), 'b') for x in st)
+# print(r)
+# print(2**14199)
+
+
+
+
+# import requests
+
+# # c = input('Enter the name of city:')
+
+# url = 'http://api.weatherapi.com/v1/current.json?key=13989793f184149a91141538230103&q=London'
+
+# r = requests.get(url)
+# print(r.text)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+### sky-fi                                                            # reading books and biographies(you know)
+#                                                                    # 9 and 10 one year
+# ml practice
+# teespring alternatives
+
+# F2:
+### invest
+# freelance
+### mobl keyboard
+# automatic water filler from direct to tank using buzzer method
+# company details checker and storer(annual report)
+
+# AI:
+# base model + customisation
+# shortly news app ai
+### real time voice translator like samsung s24 (also for youtube extension)
+### Sora:-------------------------------------------------->>>>>>>> ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓
+    # auto social meida video maker(customised or according to the preference of the user)
+    # video maker and editor using just audio(editable with text prompt) 
+    # AI youtube auto video generation(with filters)
+    # every part of video by ai (each part one by one like you can give a specific command for each part of the video ex: intemediate-3 parts)
+
+
+# ------------------------------------------------Moderatelikely:
+#/////////////////vs code icon finder
+# chapter wise experiment(practical science)
+# AI programmer (creates anything just by text prompting)(can use it in chapter wise experiment)
+# code as it is yt/screen anywhere code finder on screen
+
+# ------------------------------------------------Least likely:
+# Icon maker by ai
+# username finder
+## Book to audio book
+# notes writer by ai
+# auto web stories(ml)
+# auto blog writer
+
+
+
+# F10:
+# no sim phone(digital sim)
+# any phone pe (no same phone sim)
+#//////////// ar-vr store (3d model of any object)
+# hand gesture controlthings (tubelight)
+# Quick bank(like normal site log in)
+#//////real world physics app ai
+# thinnest phone (no sim no battery)
+#////home hair cutter easy
+# flying umbrella (sunlight protection)
+# hologram vr
+# cloud computing mode(all on cloud)
+
+
+
+
+# F20:
+# Investment management company
+# think gesture control(using think reader on throat)
+# logistics(ai transportation, own service for all companies to provide trasportation)
+# auto ai builder by taking materials from space
+# Brain clone(invincibility, selling brain(after learning something), teachers)
+### Dna data storage
+## perpetual machine(eg: pendulum)
+### water fuel vehicles
+#### Space-solar electricity rays
+# destroyer Electric shield 
+### flying cars 
+# World Scanner(like runnig a query selector for every particle on earth(like a database))
+### most strengthened material
+#### nano particles
+
+# F30:
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Can use ai to ask for some of the tasks to elaborate
+
+
+#-----Print:
+    # The Skyshop app:(Faishon Instagram: A social media like app i.e. like instagram where people can see different desings for different influencers and click on an image to buy the product shown on it. It will have a direct option of buy or add to cart.)
+            # people can buy different clothes for different profiles
+        # Cloth ai integrations:
+        # Local shopkeepers fast delivery zone(Skyshop will have local shopekeepers option for faster delivery to local people)
+                # Why they'll use Skyshop:
+                    # More reach, more popularity
+                    # A lot of Ease cause they'll just keep the products and if it gets ordered then a delivery boy will pick it and pay him the cost
+                    # no time wastage, no argument with customer, hassle free, fixed price
+                    # They will keep the whole profit of the product as usual, so there is no extra charges
+                    # calculated pricing and time according to the buyers and seller's location(the close the cheaper , the farther the expensive)
+                    # Daily user's will get it for more expensive
+                    # How will skyshop earn: The products on skyshop will be a bit expensive and that extra money will be kept by skyshop(both happy)(so the shopkeeper'll keep their normal profit and the extra profit will be taken by skyshop))
+            # Faster the product delivery, the more expensive it gets
+    # The Skyshop apps:
+        # Influencers will act as promoters.(So no ad charges)
+        # Offers:
+            # Coupons(task to do to get it)(automatic creation by ai based on the type of user)
+            # A chance to meet your influencer(if a lot of powercoins get accumulated)
+            # An option of buying a random product
+            # offers that will end soon and if the user misses the offer but wants to buy it then he will be shown the next time when the offer will get created
+        # Some Stategies       
+            # A youtuber that acts as a genuine person but is a sponser
+    # Both:
+        # 3d model by ai (realistic face 100%)(cloth face changer to real user's face)
+        ### canvas in html for AI 3d modeling
+            # create a video by face swapping with the cloth
+        # New products will be automatically created by ai based upon the type of user according to his buying preferences and those will be shown in the feeds pages
+        # Create your own design(2 methods: AI(ml model) or manually)
+            # Automatic image upscaler, cropper and adjuster etc. required to make the print look the best
+            # Upload your image to create a cloth for you in seconds
+        # Offers:
+            # Create a product:New product ideas feedback by the users, and they'll get upto ₹50000(in India) and $1000(in other countries), if their product gets hit.
+            # Gifting others(people can gift their product to other people)
+            #Review and post images of this product to get 30% off on the next product you buy(10% for rating ,... 40) 
+        # Extras:
+            # Create your own site with our subdomain on the web for local shopkeepers
+            # Showing ads and doing payments using the web(to not pay play store) or if doesn't work find another way
+
+
+
+#-----Server:(if internet gets used up, run a server on the device(or make it a server on the internet where other people can upload their information) and allow internet users to)
+
+#----------Skystore(later Playstore): ## app extension like adblock for youtube on android (play store no commison)(downloading on cloud using server method(mentioned above))
+                    ### video voice language changer(for youtube)
+                    ## auto chat bot (Whatsapp)
+                    ### browser extensions (downloader yt)
+                    # otp senders
+                    # api but like real on site(using the browser to run the api provider and post and get the prompt and completions respectively infinitely)
+                    # caption highlighter like in the ai shorts
+
+#-----AI:
+    # Model runner on site
+    # Brainery AI
+
+#-----Electricity:
+    # satellite to home rooftop
+    # satellite to direct every device
+    # back enery
+    # New type of solar panel(refletion and concentration to generate more energy)
+    # create the panels in space using the autonomous syncronised human robots or other technologies to make it able to create solar panels without human interference
+    # using propellers in space: by having some force through air movement inside a cylindrical box which applies pressure on forward walls of the cylinder and then the air is supplied back to the origin uisng a long thin pipe1
+
+
+
+# Brainery:(All these may be powered by skycity or brain energy of food or light 
+    # (A Chip in the brain Which runs using your energy by food) ( All of the certain actions may need to start to work only when a specific type of thought or word is thought or said rspctvly.)
+        # Learn things using the chip at lightning speed
+        # Download topics like an app and get that learning in brain within seconds
+        # upload your extraordinary information of a specific to pic on the brainer web(to earn)
+        # Play things(like videos, image)visually by accessing the retina, audio(by accessing the ear) even while closed eyes
+        # Communicate to each other just by thinking((thoughts will be sent as thoughts at lightning speed )automatic comprehension by the thinking algorithm to others, no need to translate the language or say the words in mind) or (create a tech that converts brain impulses to words and vice verca)
+            # Create computers that understand the thoughts language(meaning no need to convert the thoughts into words)and do the work needed
+            # ai models that understand your query by using thoughts language
+            # Call anyone, anywhere, anytime
+        # Control the Items in home(devices on iot, or that run on electricity) Extreme(Physical items by nano particles)
+        # Thought keeper notes(Store the thoughts you want in the chip and watch or use it later)
+        # Capture images ,videos or audio etc(keep the record of your whole day forever)
+        # Run internet in brain: Like internet play a real video of the thoughts and share it with others using the retina method or hologram
+        # Brain reboot
+        #Memory deleter: Delete any useless data or one that you dont like
+        # Use it for calculatinal purposes, 
+        # Use the brainery web to get the infinite data in your brain directly or search for anything at lightning speed
+        # Cloud computing using brain(run the cloud if you don't want to use your one's)
+        # Edit your brain or body in anyway you want(remove any body part anything that you don't like, change race etc)
+        # Create anything in your mind in reality by the hologram(if you want it to be realisic physically) or connect to a smart device that works with the brain and shows only what you want it to show(think of it as showing)
+        # Chemical control: Manipulate dopamine for interest in everything you want , get pleasure for anything
+        # Create clones of yourself and use it for specific purposes(can also put specific types of information bor better results about that task)
+        # Brain Query: Get all the memories and data of your brain and then ask  it for any specific information that you have in the brain using the llm ai models
+        # Controlling the physical body using the chip by manipulating the brain(like perfect bottle flips, calculated stunts etc)
+
+    #The Supergenius (100**100(in space) brains for unbelievably fastest creativity, thinking, and many more(or the supergenius brain connected to a single body of human but with nessecary parts to keep the brain alive(other uneccesary parts will be removed that doesn't serve the brain in a good purpose.)))
+        # Connect it to the chip to get supergenius powers
+        
+    #Artificial humans Get it to learn anything by uploding certain information in that artificial body that is good for humans and doesn't harm anbody (it should love humans and die to protect them) 
+        # They should run using the space electrical energy
+        # Exploit them for the best of human beings
+        # Create them in any shape so they can be a specialist of that field
+        # Creating real humans but with specific type of data
+        # Product creator for humans according to the human needs by observing or after getting any order to create something
+    # Powerful brain products:
+        # Brain AI: An ai that totally works like brain and is a master of a specific task
+        # AI assistant
+
+    # Immortal (upload your complete brain on the private account on brainery web and then create a clone(with same specifications as your normal body using the dna and genetics) body of yourself to upload the whole brain data again):
+
+    # Non-Sleeper(do the functions that are done while sleeping in the brain while waking hours or upload your brain then let it sleep there and then download the updated brain)
+
+    # DNA data storage() or in case it fails, create a data storage facility inspired by the dna storage
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Resons:
+# Age(life ends) one life one chance
+# other people have tried to create it
+# need 
+
+
+#1Gol:
+# $10B
+
+
+# Read everyday and feel;;
+# Quick dopamine(A method)
+# Instead of thinking do the task
+# Watch it(on videos or visually tp get a sense of urgency(like charging cables have radiation))
+# Motivate:
+# No other chances
+# You can be the first
+# 1 second wasted = 100hr wasted
+# you have no more than 200 days left
+# Compare to the one who is like you(towards the same thing)
+# You and only you need it
+# Can't wait bcuz someone else gotta do it
+# under 10th
+# You can have it all
+# Funding(for more projects), popularity(for marketing), trust(for people's support)
+
+
+
+
+# ToDo
+# Gain knowledge(about your topic) in free time
+# thnka dn grow rich faith
+# act as you are the master of the activity you are doing
+# Do the best thing you can do in the moment
+# THink(with focus about work while not working), focus(on work while working)
+# Always imagine what you want to become like or want to do
+
+
+
+# Thinks:
+# your age'll help you(to get to know people)
+# Work thinker
+# Don't stop
+# Love the process(bulding produts for humanity)
+# This is the perfect time and opportunity(for you now so dont think about if you fail you will do anything else )
+# Think about it visually to remember
+# Every work is equal(don't comapare or judge other's work and yours)
+# There's nothing you will love
+# Dont expect anything from others(get it on your own)
+# There's always a way for a problem(if you have the will)
+# World records(unbeatable)
+# find out the why for everything you do(maths:logic and resoning, science:future etc...)
+
+
+# To Become:
+# what is the best thing you can do right now
+# a good speaker with high-focus and unique word usage(100xbetter than musk)
+# Be urgent about every work
+# Work lover
+
+
+# Why Wake up early:
+# Allocate one major and some main tasks for a day
+# Get to the sun(Wake up before it)
+# 6 think and grow rich
+# meditation for inner peace and happiness
+# exercise(eyes, streching etc)
+# Duo
+# Drinking water for better digestive system
+
+# Would do: 
+# Birthday week plan: fast 7 days(expect many days for higher capability), disrupt product release
+
+# Reach:
+# Do you have a burning desire
+# Junun
+# create small achieveable targets
+# Allocate a task for everyday
+
+# Questions:
+# How to take the most out of you (like breaking your boat on an island to get new ways to get away, or reducing shares)
